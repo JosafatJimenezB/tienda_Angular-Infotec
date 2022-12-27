092f3c03859dec2b4d103ce7d214f781036a7bca
josafat <josa221001@gmail.com>
2022-12-26 21:21:47 -0600

    add: carga de imagenes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,16 @@ export class AppComponent {
   art = {
     codigo: 0,
     descripcion: "",
-    precio: 0
+    precio: 0,
+    imagen: ""
   }
 
   articulos = [
-    { codigo: 1, descripcion: "papas", precio: 10.55 },
-    { codigo: 2, descripcion: "manzanas", precio: 12.10 },
-    { codigo: 3, descripcion: "melon", precio: 52.30 },
-    { codigo: 4, descripcion: "cebollas", precio: 17 },
-    { codigo: 5, descripcion: "calabaza", precio: 20 }
+    { codigo: 1, descripcion: "papas", precio: 10.55, imagen: "https://res.cloudinary.com/walmart-labs/image/upload/d_default.jpg/w_960,dpr_auto,f_auto,q_auto:best/gr/images/product-images/img_large/00750081000268L.jpg"},
+    { codigo: 2, descripcion: "manzanas", precio: 12.10, imagen: "https://biotrendies.com/wp-content/uploads/2015/06/manzana.jpg" },
+    { codigo: 3, descripcion: "melon", precio: 52.30, imagen: "https://www.gob.mx/cms/uploads/image/file/403046/mel_n_1.jpg" },
+    { codigo: 4, descripcion: "cebollas", precio: 17, imagen: "https://t1.uc.ltmcdn.com/es/posts/7/7/3/como_sembrar_cebollas_20377_orig.jpg" },
+    { codigo: 5, descripcion: "calabaza", precio: 20, imagen: "https://chedrauimx.vtexassets.com/arquivos/ids/9773249/2512599_00.jpg?v=638060317903070000" },
   ];
 
   hayRegistros(){
@@ -48,21 +49,24 @@ export class AppComponent {
 
     this.articulos.push({codigo:this.art.codigo,
                          descripcion:this.art.descripcion,
-                         precio:this.art.precio});
+                         precio:this.art.precio,
+                         imagen: this.art.imagen});
 
     this.art.codigo = 0;
     this.art.descripcion = "";
     this.art.precio = 0;
+    this.art.imagen = "";
 
   }
 
 
 
 
-  seleccionar(art: { codigo: number; descripcion: string; precio: number;}){
+  seleccionar(art: { codigo: number; descripcion: string; precio: number; imagen: string;}){
     this.art.codigo = art.codigo;
     this.art.descripcion = art.descripcion;
     this.art.precio = art.precio;
+    this.art.imagen = art.imagen;
   }
 
 
@@ -71,6 +75,7 @@ export class AppComponent {
       if(this.articulos[i].codigo == this.art.codigo){
         this.articulos[i].descripcion = this.art.descripcion;
         this.articulos[i].precio = this.art.precio;
+        this.articulos[i].imagen = this.art.imagen;
         return;
       }
     }
